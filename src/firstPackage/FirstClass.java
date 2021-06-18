9b39dbdebd049d8b2756bdd015b557aa02a60452
seckinceylan <seckin.ceylan1218@gmail.com>
2021-06-17 23:07:29 -0400

    second commit

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,6 +3,7 @@ package firstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first commit");
+        System.out.println("second commit");
         
 
     }
