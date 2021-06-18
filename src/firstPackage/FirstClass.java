1843a4dd51d246e7a96ca267b2e71d44301de3da
seckinceylan <68836726+seckinceylan@users.noreply.github.com>
2021-06-18 00:49:34 -0400

    first commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first commit");
         System.out.println("second commit");
+        System.out.println("first commit from remote master branch");
 
 
     }
